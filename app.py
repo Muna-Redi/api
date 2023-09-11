543db827a1b6b98830e46888aeb3ef12fb5eb711
Muna-Redi <francischym@gmail.com>
2023-09-11 07:34:32 -0700

    jsonify with pretty print

--- app.py
+++ app.py
@@ -39,6 +39,6 @@ def api_endpoint():
             }
     if slack_name != "Munachyme" or track != "backend":
         return abort(404)
-    return jsonify(json.dumps(my_info))
+    return jsonify(my_info)
 if __name__ == "__main__":
     app.run()
